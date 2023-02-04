72e5e59799028bd842fdbd56252d7b638c6358e0
arjun-rawal <arawalarjun@gmail.com>
2023-02-04 17:50:54 -0500

    fixed spline canvas resizing

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,19 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 import { Canvas,useFrame } from '@react-three/fiber'
-import React from 'react'
+import React, {useEffect} from 'react'
 import Triangle from '@/components/triangle'
 import Spline from '@splinetool/react-spline';
+import { useRef } from 'react';
+
 // import Scene from '@/components/splined'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
   return (
-    <div className={styles.cv}>
-    <Spline  scene="https://prod.spline.design/5uxNGoicFfDdXS6W/scene.splinecode"/>
+    <div style={{width:"100vw",height:"100vh"}}>
+    <Spline scene="https://prod.spline.design/5uxNGoicFfDdXS6W/scene.splinecode" />
     </div>
   )
 }

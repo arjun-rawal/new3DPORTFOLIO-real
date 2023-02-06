f795394306557711d904a54865aa3cf1d951c83e
arjun-rawal <arawalarjun@gmail.com>
2023-02-05 22:36:44 -0500

    tried animating using javascript

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ import React, {useEffect, useRef} from 'react'
 import Triangle from '@/components/triangle'
 import Spline from '@splinetool/react-spline';
 
+
 // import Scene from '@/components/splined'
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,9 +15,12 @@ export default function Home() {
   const spline = useRef();
   const nameTag = useRef();
   const projectsTag = useRef();
+  const bottomDrawerAnimate = useRef();
   function onLoad(splineApp) {
     const Name = splineApp.findObjectByName('Name');
     const projects = splineApp.findObjectByName('Projects')
+    const bottomDrawer = splineApp.findObjectByName('board_side_3')
+    bottomDrawerAnimate.current = bottomDrawer;
     projectsTag.current = projects;
     nameTag.current = Name;
     clearAll();
@@ -28,13 +32,19 @@ export default function Home() {
     nameTag.current.visible=false;
     projectsTag.current.visible=false;
   }
+
+  function openBottomDrawer(){
+    bottomDrawerAnimate.current.emitEvent('mouseDown')
+    
+  }
+
   return (
     <div style={{width:"100vw",height:"100vh"}}>
     <Spline onLoad={onLoad} scene="https://prod.spline.design/5uxNGoicFfDdXS6W/scene.splinecode" />
     <p className={styles.name} type="button" onMouseOver={()=>{clearAll(); nameTag.current.visible=true;}}>
       Arjun Rawal
       </p>
-      <p className={styles.projects} type="button" onClick={()=>{spline.current.emitEvent('mouseHover', 'bottomDrawer');}}onMouseOver={()=>{clearAll(); projectsTag.current.visible=true;}}>
+      <p className={styles.projects} type="button" onClick={openBottomDrawer} onMouseOver={()=>{clearAll(); projectsTag.current.visible=true;}}>
       Projects
       </p>  
     </div>
